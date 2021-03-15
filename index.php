5332e00fb3c434aa578fc0155561d915c9d03052
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-14 21:54:39 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quiz 01</title>
+</head>
+<body>
+	<div>
+		<h2>Student ID: 1001821620<br>Name: Vivek Vishwanath Shetye</h2>
+	</div><br><br><br>
+
+	<div>
+		<a href="Q10.html">Q10 - Click here!</a><br><br>
+
+		<a href="Q11.html">Q11 - Click here!</a><br><br>
+
+		<a href="Q12.html">Q12 - Click here!</a><br><br>
+
+		<a href="Q13.html">Q13 - Click here!</a><br><br>
+
+		<a href="Q14.html">Q14 - Click here!</a><br><br>
+
+		<a href="#">Q15 - includes everything done above!</a><br><br>
+	</div>
+</body>
+</html>

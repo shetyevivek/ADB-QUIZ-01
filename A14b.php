936278e0c1af81ad0830670ecc31335ab8292eb1
shetyevivek <67990804+shetyevivek@users.noreply.github.com>
2021-03-15 00:59:58 -0500

    cc

--- A14b.php
+++ A14b.php
@@ -20,14 +20,16 @@ $keywords = $_POST['keywords'];
 $sql = "UPDATE data_table SET Keywords = '$keywords' WHERE Name = '$name'";
 $result = mysqli_query($con, $sql) or die('Error ' . mysqli_error($con));
 
-if($result)
+echo "Vivek";
+
+/*if($result)
 {
     echo "Keywords of " .$name. " have been successfully updated!";
 }
 else
 {
     echo $name. " does not exists!"
-}
+}*/
 
 mysqli_close($con);
 
